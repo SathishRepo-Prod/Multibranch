7cae6f746fc0f6f5caa923179c873c76a6a17cd0
SathishRepo-Prod <msathishkumar5870@gmail.com>
2026-03-23 16:09:02 +0530

    Fix Dockerfile path in docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 version=Testing
 
-sudo docker build -t sathishdevops007/busapp:${version} -f /home/ansible/app/Dockerfile
+sudo docker build -t sathishdevops007/busapp:${version} -f /home/ansible/app/dockerfile
 sudo docker push sathishdevops007/busapp:${version}
