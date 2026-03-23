175f8973de1930c03dc2be5837496701196ce11f
SathishRepo-Prod <msathishkumar5870@gmail.com>
2026-03-23 16:02:53 +0530

    Specify Dockerfile path in docker build command
    Updated docker build command to specify Dockerfile path.

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 version=Testing
 
-sudo docker build -t sathishdevops007/busapp:${version} .
+sudo docker build -t sathishdevops007/busapp:${version} -f /home/ansible/app/Dockerfile
 sudo docker push sathishdevops007/busapp:${version}
